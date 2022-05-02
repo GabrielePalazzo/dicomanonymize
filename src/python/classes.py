d8e30c99d68e456fdb1a1599497a2b5ebdd29d11
GabrielePalazzo <gabriele.palazzo6.github@gmail.com>
2022-05-02 09:52:46 +0200

    Added get functions for patients

--- src/python/classes.py
+++ src/python/classes.py
@@ -1,1 +1,10 @@
+from dataclasses import dataclass
 
+
+@dataclass
+class Patient:
+    first_name: str
+    last_name: str
+    patient_id: str
+    anonymized_id: str
+    directories: list
--- src/python/functions.py
+++ src/python/functions.py
@@ -1,4 +1,69 @@
 from pathlib import Path
+from os import listdir
+from pydicom import dcmread
+
+from classes import Patient
+
+
+def get_directories(path):
+    """
+    Get a list of subdirectories containing dicom images
+    :param path: Path
+
+    :return: list of directories containing dicom images
+    """
+
+    studies_or_patients = listdir(path)
+
+    directories_for_anonymization = []
+
+    for study_or_patient in studies_or_patients:
+        patient_images = listdir(path / study_or_patient)
+        for patient_image in patient_images:
+            patient_data = listdir(path / study_or_patient / patient_image)
+            for d in patient_data:
+                if d.endswith(".dcm"):
+                    directories_for_anonymization.append(path / study_or_patient / patient_image)
+                    break
+
+    return directories_for_anonymization
+
+
+def get_patients(lookup_directories):
+    """
+    Get list of patients to be anonymized
+    :param lookup_directories: list of directories
+
+    :return: list of Patient objects
+    """
+
+    patients = []
+
+    for d in lookup_directories:
+        files = listdir(d)
+        images = []
+        for f in files:
+            if f.endswith(".dcm"):
+                images.append(f)
+        ds = dcmread(d / images[0])
+        already_defined = False
+        for p in patients:
+            if p.patient_id == ds.PatientID:
+                already_defined = True
+                p.directories.append(d)
+                break
+        if not already_defined:
+            patients.append(
+                Patient(
+                    ds.PatientName.given_name.title(),
+                    ds.PatientName.family_name.title(),
+                    ds.PatientID,
+                    "0",
+                    [d],
+                )
+            )
+
+    return patients
 
 
 def read_patients(args):
@@ -15,5 +80,15 @@ def read_patients(args):
         # if no input directory is specified, default to current working directory
         print(f"Using {Path.cwd()} as input directory")
         input_dir = Path.cwd()
-    print(input_dir)
+
+    # for testing only
+    input_dir = Path("C:\\") / "Users" / "palazzo.gabriele" / "Downloads" / "TestMimDicom"
+
+    lookup_directories = get_directories(input_dir)
+
+    patients = get_patients(lookup_directories)
+
+    for p in patients:
+        print(p)
+
     return 0
