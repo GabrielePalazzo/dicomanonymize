e8d8e067f4e3615298bd90ed014c4a90469393c2
GabrielePalazzo <gabriele.palazzo6.github@gmail.com>
2022-05-02 09:01:37 +0200

    .

--- src/python/main.py
+++ src/python/main.py
@@ -1,4 +1,37 @@
+import argparse
+from pathlib import Path
+
+
+def parse_args(args=None):
+    """
+    Parse command line arguments
+
+    :param: args: already parsed args
+
+    :return: arguments
+    """
+
+    arg_parser = argparse.ArgumentParser(__doc__)
+
+    arg_parser.add_argument(
+        "-i",
+        "--input_directory",
+        help="Input path for the original files",
+        type=Path,
+    )
+    arg_parser.add_argument(
+        "-o",
+        "--output_directory",
+        help="Input path for the new files",
+        type=Path,
+    )
+
+    args = arg_parser.parse_args(args)
+
+    return args
 
 
 if __name__ == "__main__":
-    print("Hello!")
+    arguments = parse_args()
+
+    func.read_patients()
