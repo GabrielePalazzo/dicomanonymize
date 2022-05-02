7d51711e3f9a4e7604f2488ce125836caa678e9e
GabrielePalazzo <gabriele.palazzo6.github@gmail.com>
2022-05-02 12:57:08 +0200

    Parallelization for anonymization

--- src/python/classes.py
+++ src/python/classes.py
@@ -1,9 +1,41 @@
 from dataclasses import dataclass
 from pydicom import dcmread
 from os import listdir
+import re
+import psutil
+from multiprocessing import Pool
+from functools import partial
 
 
 VALUES_TO_ANONYMIZE = ["PatientName", "PatientID", "PatientBirthDate", "PatientSex", "PatientAge"]
+
+
+def anonymize_image(patient, path):
+    """
+    Anonymize a single image
+
+    :param path: pathlib Path to the image
+    :return: None
+    """
+
+    ds = dcmread(path)
+    for val in VALUES_TO_ANONYMIZE:
+        ds[val].value = patient.anonymized_id
+    # print(ds)
+    temp_dir = path.parent.name.lower()
+    temp_dir = re.sub(patient.last_name().lower(), patient.anonymized_id, temp_dir)
+    temp_dir = re.sub(patient.given_name().lower(), patient.anonymized_id, temp_dir)
+    path = path.parent.parent / temp_dir / path.name
+
+    temp_dir = path.parent.name.lower()
+    temp_dir = re.sub(patient.last_name().lower(), patient.anonymized_id, temp_dir)
+    path = path.parent.parent.parent / temp_dir / path.parent.name / path.name
+
+    output_path = path
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+
+    # print("Writing anonymized image", output_path)
+    ds.save_as(output_path)
 
 
 @dataclass
@@ -22,25 +54,13 @@ class Patient:
 
         self.anonymized_id = str(index)
 
-    def anonymize(self):
+    def anonymize(self, parallel=True):
         """
         Anonymize all patient data
 
+        :param parallel: use CPU multithreading
         :return: None
         """
-
-        def anonymize_image(path):
-            """
-            Anonymize a single image
-
-            :param path: pathlib Path to the image
-            :return: None
-            """
-
-            ds = dcmread(path)
-            for val in VALUES_TO_ANONYMIZE:
-                ds[val].value = self.anonymized_id
-            print(ds)
 
         images = []
         for d in self.directories:
@@ -50,10 +70,24 @@ class Patient:
                     images.append(d / f)
 
             # anonymize all images
-            for image in images:
-                anonymize_image(image)
-                break
+            if parallel:
+                cpu_number = max(psutil.cpu_count() - 2, 1)
+                with Pool(cpu_number) as p:
+                    p.map(partial(anonymize_image, self), images)
+            else:
+                for image in images:
+                    anonymize_image(self, image)
+                # break
         # print(self)
+
+    def given_name(self):
+        """
+        Patient given name
+
+        :return: str containing patient's given name
+        """
+
+        return self.patient_data["PatientName"].given_name.title()
 
     def last_name(self):
         """
