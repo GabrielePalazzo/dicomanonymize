48f5e43483d99ff65c20d1ee25ae4d753da27f8f
GabrielePalazzo <gabriele.palazzo6.github@gmail.com>
2022-05-03 16:00:31 +0200

    Easier to import

--- src/dicomanonymize/scripts/dicomanonymize_script.py
+++ src/dicomanonymize/scripts/dicomanonymize_script.py
@@ -3,7 +3,12 @@ import os.path
 from pathlib import Path
 import time
 
-from dicomanonymize.anonymizer import anonymize
+from dicomanonymize.functions import (
+    read_patients,
+    anonymize_id_patients,
+    anonymize_patients,
+    write_conversion_table,
+)
 
 
 def parse_args(args=None):
@@ -38,6 +43,26 @@ def parse_args(args=None):
     args = arg_parser.parse_args(args)
 
     return args
+
+
+def anonymize(input_directory, output_directory=None, parallel=True):
+    """
+    Anonymize patients data
+
+    :param input_directory: Path of the input directory
+    :param output_directory: Path of the output directory
+    :param parallel: use CPU multithreading
+    :return: None
+    """
+
+    if output_directory is None:
+        output_directory = input_directory
+    output_directory.mkdir(parents=True, exist_ok=True)
+
+    patients = read_patients(input_directory)
+    anonymize_id_patients(patients)
+    anonymize_patients(output_directory, patients, parallel)
+    write_conversion_table(output_directory, patients)
 
 
 def main():
