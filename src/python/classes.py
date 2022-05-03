72eda42c4bd193e6d89a6c700491dec768e69eb8
GabrielePalazzo <gabriele.palazzo6.github@gmail.com>
2022-05-03 08:59:32 +0200

    Switched to ThreadPool

--- src/python/classes.py
+++ src/python/classes.py
@@ -3,7 +3,7 @@ from pydicom import dcmread
 from os import listdir
 import re
 import psutil
-from multiprocessing import Pool
+from multiprocessing.pool import ThreadPool
 from functools import partial
 
 
@@ -23,38 +23,6 @@ VALUES_TO_ANONYMIZE = [
     "ContentTime",
     "AccessionNumber",
 ]
-
-
-def anonymize_image(patient, output_dir, path):
-    """
-    Anonymize a single image
-
-    :param patient: Patient to be anonymized
-    :param output_dir: Path of the output directory
-    :param path: pathlib Path to the image
-    :return: None
-    """
-
-    ds = dcmread(path)
-    for val in VALUES_TO_ANONYMIZE:
-        try:
-            ds[val].value = patient.anonymized_id
-        except Exception:
-            print(f"{val} not found in {path}")
-    # print(ds)
-    temp_dir = path.parent.name.lower()
-    temp_dir = re.sub(patient.last_name().lower(), patient.anonymized_id, temp_dir)
-    temp_dir = re.sub(patient.given_name().lower(), patient.anonymized_id, temp_dir)
-    temp_dir = re.sub("_[0-9]{4}-[0-9]{2}-[0-9]{2}_", f"_{patient.anonymized_id}_", temp_dir)
-    path = path.parent.parent / temp_dir / path.name
-
-    temp_dir = path.parent.parent.name.lower()
-    temp_dir = re.sub(patient.last_name().lower(), patient.anonymized_id, temp_dir)
-    output_path = output_dir / temp_dir / path.parent.name / path.name
-    output_path.parent.mkdir(parents=True, exist_ok=True)
-
-    # print("Writing anonymized image", output_path)
-    ds.save_as(output_path)
 
 
 @dataclass
@@ -82,6 +50,37 @@ class Patient:
         :return: None
         """
 
+        def anonymize_image(output_directory, path):
+            """
+            Anonymize a single image
+
+            :param patient: Patient to be anonymized
+            :param output_dir: Path of the output directory
+            :param path: pathlib Path to the image
+            :return: None
+            """
+
+            ds = dcmread(path)
+            for val in VALUES_TO_ANONYMIZE:
+                try:
+                    ds[val].value = self.anonymized_id
+                except Exception:
+                    print(f"{val} not found in {path}")
+            # print(ds)
+            temp_dir = path.parent.name.lower()
+            temp_dir = re.sub(self.last_name().lower(), self.anonymized_id, temp_dir)
+            temp_dir = re.sub(self.given_name().lower(), self.anonymized_id, temp_dir)
+            temp_dir = re.sub("_[0-9]{4}-[0-9]{2}-[0-9]{2}_", f"_{self.anonymized_id}_", temp_dir)
+            path = path.parent.parent / temp_dir / path.name
+
+            temp_dir = path.parent.parent.name.lower()
+            temp_dir = re.sub(self.last_name().lower(), self.anonymized_id, temp_dir)
+            output_path = output_directory / temp_dir / path.parent.name / path.name
+            output_path.parent.mkdir(parents=True, exist_ok=True)
+
+            # print("Writing anonymized image", output_path)
+            ds.save_as(output_path)
+
         images = []
         for d in self.directories:
             files = listdir(d)
@@ -91,12 +90,12 @@ class Patient:
 
             # anonymize all images
             if parallel:
-                cpu_number = max(psutil.cpu_count() - 2, 1)
-                with Pool(cpu_number) as p:
-                    p.map(partial(anonymize_image, self, output_dir), images)
+                cpu_number = max(psutil.cpu_count(), 1)
+                with ThreadPool(cpu_number) as p:
+                    p.map(partial(anonymize_image, output_dir), images)
             else:
                 for image in images:
-                    anonymize_image(self, output_dir, image)
+                    anonymize_image(output_dir, image)
 
     def given_name(self):
         """
