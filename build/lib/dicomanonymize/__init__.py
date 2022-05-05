95d67ae3df29d8ed11f58ebcdc10b353cdbe943c
GabrielePalazzo <73099233+GabrielePalazzo@users.noreply.github.com>
2022-05-05 09:14:15 +0200

    Delete build/lib/dicomanonymize directory

--- build/lib/dicomanonymize/__init__.py
+++
@@ -1,6 +0,0 @@
-""" dicomanonymize """
-
-from dicomanonymize.functions import anonymize, read_patients  # noqa: F401
-from dicomanonymize.classes import Patient  # noqa: F401
-
-__version__ = "0.0.2"
